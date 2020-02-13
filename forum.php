11f6a3b88b46566de90c87ef9c76c83214404861
tibo <thibaut.schlick@yahoo.com>
2020-02-13 16:25:45 -0500

    added a new file

---
+++ forum.php
@@ -0,0 +1,32 @@
+	<html>
+   
+	<head>
+		<title></title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="" />
+	</head>
+	<body class="right-sidebar is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<section id="header">
+					<div class="container">
+						<!-- Nav -->
+						 <?php include("forum_menus.php"); 
+						 
+						
+							
+							require('forum_view.php');
+						?> 
+							
+					</div>
+				</section>
+
+		
+
+	</body>
+</html>
+
+
+
